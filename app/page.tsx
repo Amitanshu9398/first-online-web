06f1cc0db8714abeb6f385546048eecad8d2f38c
Amitanshu <amitanshu9398@gmail.com>
2025-04-10 21:39:02 +0530

    background added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import Squares from "@/components/Squarebackground";
+
 export default function Home() {
   return (
     <main>
       <div>
+        <Squares
+          speed={0.5}
+          squareSize={40}
+          direction="diagonal" // up, down, left, right, diagonal
+          borderColor="#fff"
+          hoverFillColor="#222"
+        />
         <h1>Hello World</h1>
       </div>
     </main>
